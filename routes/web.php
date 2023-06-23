25b2d30297d0abf2424ff4bded33dc17e3f6982e
shlomo shalev ben aderet <shlomo.shalev.career@gmail.com>
2023-06-23 16:06:39 +0300

    order countries by last ids

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return Inertia::render('Dashboard', [
-        'countries' => Country::select('name', 'iso', 'id')->get(),
+        'countries' => Country::select('name', 'iso', 'id')->orderBy('id', 'desc')->get(),
     ]);
 })->middleware(['auth', 'verified'])->name('dashboard');
 
